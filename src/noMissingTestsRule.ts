8018147899ab029609deddd49ae45f3a91db0cd4
Nathan Dench <ndenc2@gmail.com>
2019-03-13 17:14:54 +1000

    feat(rule): Update to Typescript 3

--- src/noMissingTestsRule.ts
+++ src/noMissingTestsRule.ts
@@ -1,26 +1,47 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import * as Lint from 'tslint';
 import * as ts from 'typescript';
 
 export class Rule extends Lint.Rules.AbstractRule {
     public static FAILURE_STRING = 'Missing test case';
 
+    public static metadata: Lint.IRuleMetadata = {
+        ruleName: 'no-missing-tests',
+        description: 'Ensure that all ts files have a test',
+        optionsDescription:
+            'A list of method names to ban. If no method names are provided, all console methods are banned.',
+        options: {
+            type: 'object',
+            properties: {
+                srcBase: {
+                    type: 'string',
+                },
+                testBase: {
+                    type: 'string',
+                },
+            },
+        },
+        optionExamples: [[true, 'log', 'error']],
+        type: 'functionality',
+        typescriptOnly: false,
+    };
+
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        return this.applyWithFunction(sourceFile, walk);
+        return this.applyWithFunction(sourceFile, walk, this.ruleArguments[0]);
     }
 }
 
-function walk(ctx: Lint.WalkContext<void>) {
-    ts.forEachChild(ctx.sourceFile, recur);
-
-    function recur(node: ts.Node): void {
-        if (!isSourceFile(node)) {
-            return;
-        }
-
-        ctx.addFailureAtNode(node, Rule.FAILURE_STRING);
-    }
+interface IRuleOptions {
+    srcBase: string;
+    testBase: string;
 }
 
-function isSourceFile(node: ts.Node): node is ts.SourceFile {
-    return (node.kind === ts.SyntaxKind.SourceFile);
+function walk(ctx: Lint.WalkContext<IRuleOptions>) {
+    const relativePath = path.relative(process.cwd(), ctx.sourceFile.fileName).replace(ctx.options.srcBase, '');
+    const basename = relativePath.replace(path.parse(relativePath).ext, '');
+    const testFile = `${path.join(ctx.options.testBase, basename)}.spec.js`;
+    if (!fs.existsSync(testFile)) {
+        ctx.addFailureAtNode(ctx.sourceFile, Rule.FAILURE_STRING);
+    }
 }
